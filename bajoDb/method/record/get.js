525a654cc5901d8be95646ec73c360097afecdb0
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-08 21:50:53 +0700

    Updates

--- bajoDb/method/record/get.js
+++ bajoDb/method/record/get.js
@@ -5,7 +5,7 @@ async function get ({ schema, id, options = {} } = {}) {
   const { thrownNotFound = true } = options
   const coll = instance.db.collection(schema.repoName)
   const result = await coll.findOne({ _id: id })
-  if (!result && thrownNotFound) throw error('Record \'%s@%s\' not found!', id, schema.name)
+  if (!result && thrownNotFound) throw error('Record \'%s@%s\' not found!', id, schema.name, { statusCode: 404 })
   return { data: result }
 }
 
