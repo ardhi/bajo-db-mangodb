126af715d82886c989cc766349820c0e65fc737c
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-16 07:11:57 +0700

    Updates

--- bajoDb/boot/instantiation.js
+++ bajoDb/boot/instantiation.js
@@ -5,7 +5,7 @@ import repoExists from '../method/repo/exists.js'
 async function instantiation ({ connection, schemas, noRebuild }) {
   const { importPkg } = this.bajo.helper
   const { pick } = await importPkg('lodash-es')
-  this.bajoDbMongodb.instances = this.bajoDbMongodb.instances || []
+  this.bajoDbMongodb.instances = this.bajoDbMongodb.instances ?? []
   const instance = pick(connection, ['name', 'type'])
   let url = connection.url
   if (!url) {
@@ -13,7 +13,7 @@ async function instantiation ({ connection, schemas, noRebuild }) {
     if (connection.user) url += `${connection.user}:${connection.password}@`
     url += `${connection.host}:${connection.port}`
   }
-  instance.client = new MongoClient(url, connection.options || {})
+  instance.client = new MongoClient(url, connection.options ?? {})
   instance.db = instance.client.db(connection.database)
   this.bajoDbMongodb.instances.push(instance)
   if (noRebuild) return
--- bajoDb/boot/sanitizer.js
+++ bajoDb/boot/sanitizer.js
@@ -2,8 +2,8 @@ async function sanitizer (conn) {
   const { importPkg, fatal } = this.bajo.helper
   const { pick } = await importPkg('lodash-es')
   if (!conn.url) {
-    conn.host = conn.host || 'localhost'
-    conn.port = conn.port || 27017
+    conn.host = conn.host ?? 'localhost'
+    conn.port = conn.port ?? 27017
   }
   if (!conn.database) fatal('\'%s@%s\' key is required', 'database', conn.name, { code: 'BAJODBMONGODB_MISSING_CONNECTION_DATABASE', payload: conn })
   const result = pick(conn, ['type', 'name', 'driver', 'host', 'port', 'user', 'password', 'database'])
