5f3e4a83ff7d5fe48f800e2d50d6c5616c41ee98
Ardhi Lukianto <ardhi@lukianto.com>
2024-04-24 19:45:15 +0700

    Updates

--- bajoDb/method/record/find.js
+++ bajoDb/method/record/find.js
@@ -3,8 +3,8 @@ async function find ({ schema, filter = {}, options = {} } = {}) {
   const { omit } = this.bajo.helper._
   const { instance } = getInfo(schema)
   const { prepPagination } = this.bajoDb.helper
-  const { limit, skip, query, sort, page } = await prepPagination(filter, schema)
-  const criteria = query ?? {}
+  const { limit, skip, sort, page } = await prepPagination(filter, schema)
+  const criteria = filter.query ?? {}
   const coll = instance.db.collection(schema.collName)
   let count = 0
   if (options.count && !options.dataOnly) count = await coll.countDocuments(criteria)
--- bajoDb/method/stat/count.js
+++ bajoDb/method/stat/count.js
@@ -1,9 +1,7 @@
 async function count ({ schema, filter = {}, options = {} }) {
   const { getInfo } = this.bajoDb.helper
   const { instance } = getInfo(schema)
-  const { prepPagination } = this.bajoDb.helper
-  const { query } = await prepPagination(filter, schema)
-  const criteria = query ?? {}
+  const criteria = filter.query ?? {}
   const coll = instance.db.collection(schema.collName)
   const count = await coll.countDocuments(criteria)
   return { data: count }
